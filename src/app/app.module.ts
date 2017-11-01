13da74ad02523a440988263edbec64d64a761b35
Elko van den Busken <elko@TDA-1004-2.TDA>
2017-11-01 08:45:07 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeamFormComponent } from './team-form/team-form.component';
+import { TeamListComponent } from './team-list/team-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamFormComponent,
+    TeamListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/team-form/team-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'team-form',
+  templateUrl: './team-form.component.html',
+  styleUrls: ['./team-form.component.css']
+})
+export class TeamFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
